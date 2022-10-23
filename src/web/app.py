6b164836f232f2579123c71d0455c4aa8bc134fe
jyotirmoy <jyotirmoysamadder712@gmail.com>
2022-10-23 22:17:50 +0530

    dev setup

--- src/web/app.py
+++ src/web/app.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template
-from db import get_db, close_db
-import sqlalchemy
-from logger import log
+# from db import get_db, close_db
+# import sqlalchemy
+# from logger import log
 
 app = Flask(__name__)
-app.teardown_appcontext(close_db)
+# app.teardown_appcontext(close_db)
 
 
 @app.route("/")
@@ -12,21 +12,24 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/health")
-def health():
-    log.info("Checking /health")
-    db = get_db()
-    health = "BAD"
-    try:
-        result = db.execute("SELECT NOW()")
-        result = result.one()
-        health = "OK"
-        log.info(f"/health reported OK including database connection: {result}")
-    except sqlalchemy.exc.OperationalError as e:
-        msg = f"sqlalchemy.exc.OperationalError: {e}"
-        log.error(msg)
-    except Exception as e:
-        msg = f"Error performing healthcheck: {e}"
-        log.error(msg)
+# @app.route("/health")
+# def health():
+#     log.info("Checking /health")
+#     db = get_db()
+#     health = "BAD"
+#     try:
+#         result = db.execute("SELECT NOW()")
+#         result = result.one()
+#         health = "OK"
+#         log.info(f"/health reported OK including database connection: {result}")
+#     except sqlalchemy.exc.OperationalError as e:
+#         msg = f"sqlalchemy.exc.OperationalError: {e}"
+#         log.error(msg)
+#     except Exception as e:
+#         msg = f"Error performing healthcheck: {e}"
+#         log.error(msg)
 
-    return health
+#     return health
+
+if __name__ == '__main__':
+   app.run()
